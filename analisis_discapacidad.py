07644f0f4637c4a4e8e190655acc913674966b05
Emerson Rojas Velasquez <er58933@gmail.com>
2024-11-05 10:40:18 -0500

    Update analisis_discapacidad.py

--- analisis_discapacidad.py
+++ analisis_discapacidad.py
@@ -16,6 +16,14 @@ import matplotlib.pyplot as plt
 plt.figure(figsize=(10, 6))
 df_discapacitado['CAUSA'].hist(bins=20, edgecolor='black')
 plt.title("Distribucion de causas de discapacidad en los postulantes", fontsize=16)
-plt.xlabel("Cantidad")
-plt.ylabel("Causas")
+plt.xlabel("Causas")
+plt.ylabel("Cantidad")
+plt.show()
+
+#2. Distribucion de la dificultad que causaria la discapacidad
+plt.figure(figsize=(10, 6))
+df_discapacitado['DSCORE'].hist(bins=20, edgecolor='black')
+plt.title("Distribucion de la dificultad que causaria la discapacidad", fontsize=16)
+plt.xlabel("1= Sin dificultad, 2 = Dificultad leve o moderada, 3 = Dificultad severa")
+plt.ylabel("Cantidad")
 plt.show()
